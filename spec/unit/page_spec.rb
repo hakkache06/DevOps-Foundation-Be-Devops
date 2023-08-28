a7261ba4dba2ed7d62ad6c6d16d36c6fdb91934a
hakkache06 <hakkacheyassin@gmail.com>
2023-08-28 06:56:56 +0100

    Add Selenuim

--- spec/unit/page_spec.rb
+++ spec/unit/page_spec.rb
@@ -1,7 +1,13 @@
 require "capybara"
 require 'capybara/dsl'
+require 'selenium-webdriver'
 
+include Capybara::DSL
+Capybara.register_driver :selenium do |app|
+end
 describe "Example age render until test" do 
     it "it should show explore logo" do
+    visit('/')
+    expect(page.has_selector? 'a.logo').to be true
     end
 end
